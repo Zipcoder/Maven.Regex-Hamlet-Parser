b120bbb05b4d6ed612caba9ae5f7c0645af78a44
Zachary Kitto <zmkitto11@gmail.com>
2021-08-01 04:39:15 -0400

    followed instructions

--- src/test/java/HamletParserTest.java
+++ src/test/java/HamletParserTest.java
@@ -1,5 +1,9 @@
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static org.junit.Assert.*;
 
@@ -15,17 +19,69 @@ public class HamletParserTest {
 
     @Test
     public void testChangeHamletToLeon() {
+        // When
+        hamletParser.changeHamletToLeon();
+        boolean hasHamlet = hamletParser.findHamlet();
+
+        // Then
+        Assert.assertFalse(hasHamlet);
     }
 
     @Test
     public void testChangeHoratioToTariq() {
+        // When
+        hamletParser.changeHoratioToTariq();
+        boolean hasHoratio = hamletParser.findHoratio();
+
+        // Then
+        Assert.assertFalse(hasHoratio);
     }
 
     @Test
     public void testFindHoratio() {
+        // Given
+        boolean expected = Pattern.compile("(?i)Horatio").matcher(hamletText).find();
+
+        // When
+        boolean actual = hamletParser.findHoratio();
+
+        // Then
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void testFindHamlet() {
+        // Given
+        boolean expected = Pattern.compile("(?i)Hamlet").matcher(hamletText).find();
+
+        // When
+        boolean actual = hamletParser.findHamlet();
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testMatchHamlet() {
+        // Given
+        String expectedMatcher = Pattern.compile("(?i)Hamlet").matcher(hamletText).toString();
+
+        // When
+        String actualMatcher = hamletParser.matchHamlet().toString();
+
+        // Then
+        Assert.assertEquals(expectedMatcher, actualMatcher);
+    }
+
+    @Test
+    public void testMatchHoratio() {
+        // Given
+        String expectedMatcher = Pattern.compile("(?i)Horatio").matcher(hamletText).toString();
+
+        // When
+        String actualMatcher = hamletParser.matchHoratio().toString();
+
+        // Then
+        Assert.assertEquals(expectedMatcher, actualMatcher);
     }
 }
